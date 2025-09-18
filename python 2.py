162c34fa9e789f5edc883498266e2418bca6d2a0
piyushnegi821818-dev <piyushnegi821818@gmail.com>
2025-09-18 11:49:26 +0530

    Add files via upload

---
+++ python 2.py
@@ -0,0 +1,16 @@
+'''def name(a,b):
+
+    print(a+b)
+name(4,5)
+name(66,69)'''
+#num=int input("num:"))
+def calculator(a,b,operator):
+ if operator =="+":
+     print(a+b)
+ elif operator =="-":
+     print(a-b)
+ elif operator =="*":
+     print(a*b)
+ elif operator =="/":
+     print(a/b)
+calculator(100,100,"/")
